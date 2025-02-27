c8a094a782259b09634c6d344445e66457a9c931
Novedh <devonhuang11@gmail.com>
2025-02-27 14:29:48 -0800

    add temp image to left column

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ProfileImage from "./assets/images/temp.jpeg";
 function App() {
   let items = ["list 1", "list 2", "list 3", "list 4"];
   const handleSelectItem = (item: string) => {
@@ -29,25 +30,37 @@ function App() {
     <div className="container-fluid">
       <div className="row">
         <div
-          className="col-6 bg-danger bg-gradient"
+          className="col-6 bg-danger bg-gradient d-flex justify-content-center align-items-center"
           style={{ minHeight: "100vh" }}
-        ></div>
-        <div className="col-6 bg-white" style={{ minHeight: "100vh" }}>
-          <ListGroup
-            items={items}
-            heading="Devon Huang"
-            onSelectItem={handleSelectItem}
+        >
+          <img
+            src={ProfileImage}
+            alt="Profile"
+            className="img-fluid"
+            style={{ maxWidth: "60%" }}
           />
-          <div className="d-flex gap-2 justify-content-center align-items-center h-100">
-            {socialLinks.map((link, index) => (
-              <Button
-                key={index}
-                color={link.color}
-                onClick={() => handleLinkClick(link.url)}
-              >
-                {link.name}
-              </Button>
-            ))}
+        </div>
+        <div
+          className="col-6 bg-white d-flex flex-column justify-content-center align-items-center"
+          style={{ minHeight: "100vh" }}
+        >
+          <div className="text-center">
+            <ListGroup
+              items={items}
+              heading="Devon Huang"
+              onSelectItem={handleSelectItem}
+            />
+            <div className="d-flex gap-2 justify-content-center align-items-center mt-3">
+              {socialLinks.map((link, index) => (
+                <Button
+                  key={index}
+                  color={link.color}
+                  onClick={() => handleLinkClick(link.url)}
+                >
+                  {link.name}
+                </Button>
+              ))}
+            </div>
           </div>
         </div>
       </div>
